f12db464398ed8e5745dcdef8afc678a4d36c081
Hazel Viswanath <viswanath.hazel@gmail.com>
2024-12-15 13:22:40 -0700

    Bootstrap and tutorial from jetbrains

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from datetime import datetime
+import geocoder
+import requests
+from django.http import HttpResponse
 
-# Create your views here.
+
+def temp_here(request):
+    location = geocoder.ip('me').latlng
+    endpoint = "https://api.open-meteo.com/v1/forecast"
+    api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m"
+    now = datetime.now()
+    hour = now.hour
+    meteo_data = requests.get(api_request).json()
+    temp = meteo_data['hourly']['temperature_2m'][hour]
+    return HttpResponse(f"Here it's {temp}")
--- Website/urls.py
+++ Website/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.include import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include('meteo.urls')),
 ]
